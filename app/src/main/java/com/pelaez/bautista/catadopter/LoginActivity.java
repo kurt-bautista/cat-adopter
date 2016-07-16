6eabe8e66557a3d621ffa06667ff2f97c82f2a15
Kurt Vincent Bautista <kurt.v@outlook.com>
2016-07-16 16:43:12 +0800

    Created activity_register

--- app/src/main/java/com/pelaez/bautista/catadopter/LoginActivity.java
+++ app/src/main/java/com/pelaez/bautista/catadopter/LoginActivity.java
@@ -2,12 +2,25 @@ package com.pelaez.bautista.catadopter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v) {
+
+    }
+
+    public void register(View v) {
+
     }
 }
