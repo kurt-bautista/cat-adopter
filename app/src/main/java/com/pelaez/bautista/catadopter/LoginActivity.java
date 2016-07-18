6d6c48f3e0d9bd3a32537790bd6ccfd7df0d6c2e
Kurt Vincent Bautista <kurt.v@outlook.com>
2016-07-18 19:05:34 +0800

    Finished activity_register layout

--- app/src/main/java/com/pelaez/bautista/catadopter/LoginActivity.java
+++ app/src/main/java/com/pelaez/bautista/catadopter/LoginActivity.java
@@ -28,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTitle("Login");
 
         email = (EditText)findViewById(R.id.emailTextBox);
         password = (EditText)findViewById(R.id.passwordTextBox);
--- app/src/main/java/com/pelaez/bautista/catadopter/RegisterActivity.java
+++ app/src/main/java/com/pelaez/bautista/catadopter/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.pelaez.bautista.catadopter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setTitle("Create a new account");
+    }
+
+    public void createAccount(View v) {
+
     }
 }
