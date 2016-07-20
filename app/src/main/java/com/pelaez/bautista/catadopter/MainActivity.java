44e0be9e4f99d6bdaffaf3f9313099475186eae0
Kurt Vincent Bautista <kurt.v@outlook.com>
2016-07-20 09:49:44 +0800

    Basic drawer stuff

--- app/src/main/java/com/pelaez/bautista/catadopter/MainActivity.java
+++ app/src/main/java/com/pelaez/bautista/catadopter/MainActivity.java
@@ -1,22 +1,41 @@
 package com.pelaez.bautista.catadopter;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView navMenu;
-    private ArrayAdapter<String> navAdapter;
+    private String[] screens;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        navMenu = (ListView)findViewById(R.id.listView);
-        navAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[] {"one", "two", "three"});
-        navMenu.setAdapter(navAdapter);
+        screens = getResources().getStringArray(R.array.screens_array);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.mainDrawerLayout);
+        mDrawerList = (ListView)findViewById(R.id.mainDrawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, screens));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+
     }
 }
